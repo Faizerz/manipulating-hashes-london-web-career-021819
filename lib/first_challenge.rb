3f068bffa4f3a87acf88edaae712b02c77f03727
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 23:59:40 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   contacts.each do |name, data|
     data.each do |attribute, values|
       if attribute == :favorite_icecream_flavors
-        :favorite_icecream_flavors.delete_if("strawberry")
+        attribute.delete_if("strawberry")
       end
     end
   end
